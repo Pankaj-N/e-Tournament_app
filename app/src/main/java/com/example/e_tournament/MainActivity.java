5b505eeadc3aa373320ffd3b5fbee782c30ff74b
Pankaj-N <56391327+Pankaj-N@users.noreply.github.com>
2020-02-27 03:42:16 +0000

    FrontEnd of Homepage and All button created!

--- app/src/main/java/com/example/e_tournament/MainActivity.java
+++ app/src/main/java/com/example/e_tournament/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.e_tournament;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,8 +9,12 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -22,19 +27,68 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new Homepage()).commit();
+            navigationView.setCheckedItem(R.id.nav_home);
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new Homepage()).commit();
+                break;
+            case R.id.nav_create:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CreateTournament()).commit();
+                break;
+            case R.id.nav_join:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new JoinTournament()).commit();
+                break;
+            case R.id.nav_player:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddPlayer()).commit();
+                break;
+            case R.id.nav_team:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AddTeam()).commit();
+                break;
+            case R.id.nav_past:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PastTournament()).commit();
+                break;
+            case R.id.nav_help:
+                Toast.makeText(this, "Downloding..", Toast.LENGTH_LONG).show();
+                break;
+            case R.id.nav_logout:
+                Toast.makeText(this, "Logging off", Toast.LENGTH_LONG).show();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
     public void onBackPressed() {
 
-        if(drawer.isDrawerOpen(GravityCompat.START)){
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-        }else{
+        } else {
             super.onBackPressed();
         }
 
